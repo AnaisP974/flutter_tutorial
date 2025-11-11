ceefd2ce92b5c126e5e731361df5c9e6837bfea9
AnaisP974 <anaisperigny31@gmail.com>
2025-11-11 17:45:53 +0100

    Menu Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -25,13 +25,51 @@ class HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary, // Changer la couleur en restant dans le même théme couleur déjà prédéfini
-        leading: Icon(Icons.menu),
+        // Ce que nous aurions fait :
+        // leading: IconButton(onPressed: (){}, icon: Icon(Icons.menu)),
         title: Center(child: Text("Tutorial")),
         elevation: 14.0,
         actions: [
           IconButton(onPressed: () {}, icon: Icon(Icons.search)),
           IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
         ],
+      ),
+      // Ce qu'il faut faire !
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            // Header
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text("Menu"),
+            ),
+            // Liste des éléments du menu, cliquable
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text(("Parameter")),
+              subtitle: Text("Make your custom settings"),
+              trailing: Icon(Icons.chevron_right),
+              onTap: (){},
+            ),
+            ListTile(
+              leading: Icon(Icons.account_box),
+              title: Text(("Profil")),
+              onTap: (){},
+            ),
+            ListTile(
+              leading: Icon(Icons.help),
+              title: Text(("Help")),
+              onTap: (){},
+            ),
+            ListTile(
+              leading: Icon(Icons.close),
+              title: Text(("Close")),
+              onTap: (){},
+            )
+          ],
+        ),
       ),
       body: Center(child: Text("Flutter")),
     );
