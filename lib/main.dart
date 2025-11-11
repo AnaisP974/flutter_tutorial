54549594b4d720dd26bdb3e56ab1a6e44d97dcf5
AnaisP974 <anaisperigny31@gmail.com>
2025-11-11 17:26:45 +0100

    Step8: AppBar()

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  // Entry point of the application
   runApp(
     MaterialApp(
-      // Personnalisation du theme de la page
-      theme: ThemeData(
-        colorSchemeSeed: Colors.blue,
-        scaffoldBackgroundColor: Colors.white,
-      ),
-      // Retrait de la bannière "Debug"
+      theme: ThemeData(colorSchemeSeed: const Color.fromARGB(255, 13, 99, 169)),
       debugShowCheckedModeBanner: false,
-      // Lancement de la HomePage
       home: const HomePage(),
     ),
   );
@@ -27,74 +20,20 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
-  Color bg = Colors.white;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: bg,
-      appBar: AppBar(title: Center(child: Text("Les différents boutons"))),
-      body: ListView(
-        padding: const EdgeInsets.all(40),
-        children: [
-          OutlinedButton(
-            onPressed: () {
-              setState(() {
-                bg = Colors.blue;
-              });
-            },
-            style: OutlinedButton.styleFrom(backgroundColor: Colors.blueAccent),
-            child: const Text("Blue"),
-          ),
-          const SizedBox(height: 15),
-          TextButton(
-            onPressed: () {
-              setState(() {
-                bg = const Color.fromARGB(148, 194, 33, 243);
-              });
-            },
-            // Pour mettre du style sur le bouton
-            style: TextButton.styleFrom(
-              backgroundColor: const Color.fromARGB(255, 216, 46, 243),
-            ),
-            child: const Text("Pink"),
-          ),
-          const SizedBox(height: 15),
-          IconButton(
-            onPressed: () {
-              setState(() {
-                bg = const Color.fromARGB(147, 243, 236, 33);
-              });
-            },
-            style: IconButton.styleFrom(backgroundColor: Colors.yellowAccent),
-            icon: Icon(Icons.home),
-          ),
-          const SizedBox(height: 15),
-          ElevatedButton(
-            onPressed: () {
-              setState(() {
-                bg = Colors.white;
-              });
-            },
-            // Pour mettre du style sur le bouton
-            style: ElevatedButton.styleFrom(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadiusGeometry.circular(1),
-              ),
-              elevation: 12,
-            ),
-            child: Text("Réinitialiser"),
-          ),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary, // Changer la couleur en restant dans le même théme couleur déjà prédéfini
+        leading: Icon(Icons.menu),
+        title: Center(child: Text("Tutorial")),
+        elevation: 14.0,
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            bg = const Color.fromARGB(146, 54, 243, 33);
-          });
-        },
-        child: const Icon(Icons.add),
-      ),
+      body: Center(child: Text("Flutter")),
     );
   }
 }
