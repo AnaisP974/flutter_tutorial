2efed24fffbcd432504968b58a102d891604fbdb
AnaisP974 <anaisperigny31@gmail.com>
2025-11-11 16:09:25 +0100

    Step6: bottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:tuto2/pages/actus.dart';
+import 'package:tuto2/pages/communautes.dart';
+import 'package:tuto2/pages/appels.dart';
+import 'package:tuto2/pages/discussions.dart';
 
 void main() {
   // Entry point of the application
-  runApp(const MaterialApp(home: HomePage()));
+  runApp(
+    MaterialApp(
+      // Personnalisation du theme de la page
+      theme: ThemeData(
+        colorSchemeSeed: Colors.green,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      // Retrait de la bannière "Debug"
+      debugShowCheckedModeBanner: false,
+      // Lancement de la HomePage
+      home: const HomePage(),
+    ),
+  );
 }
 
 class HomePage extends StatefulWidget {
@@ -15,68 +31,56 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
-  List<String> perfumeBrands = [
-    "Chanel",
-    "Dior",
-    "Gucci",
-    "Versace",
-    "Yves Saint Laurent",
-    "Tom Ford",
-    "Hermès",
-    "Guerlain",
-    "Lancôme",
-    "Calvin Klein",
-    "Armani",
-    "Givenchy",
-    "Prada",
-    "Burberry",
-    "Montblanc",
-    "Hugo Boss",
-    "Dolce & Gabbana",
-    "Jimmy Choo",
-    "Jo Malone London",
-    "Creed",
-    "Maison Francis Kurkdjian",
-    "Byredo",
-    "Acqua di Parma",
-    "Issey Miyake",
-    "Elie Saab",
-    "Salvatore Ferragamo",
-    "Armaf",
-    "Lattafa",
-    "Roberto Cavalli",
-    "Valentino",
-    "Ariana Grande Fragrances",
-    "Versace Dylan Blue (brand instead maybe Versace)",
-    "Kenzo",
-    "Mugler",
-    "Nina Ricci",
-    "Cacharel",
-    "Bvlgari",
-    "Paco Rabanne",
-    "Carolina Herrera",
-    "Coach",
-    "Tommy Hilfiger",
-    "Jean Paul Gaultier"
+  // Stockage des pages dans l'ordre d'affichage
+  final pages = [
+    DiscussionsPage(),
+    ActusPage(),
+    CommunautesPage(),
+    AppelsPage(),
   ];
+
+  // Initialisation de la variable pageIndex à 0
+  int pageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
+      // **** Barre de Navigation ****
       appBar: AppBar(
-        title: Center(child: const Text('ListView() Widget')),
-        elevation: 12.0,
+        title: Center(child: const Text('WHATSAPP')),
+        elevation: 12.0, // permet l'ajout d'un filtre
       ),
-      body: ListView.separated(
-        itemCount: perfumeBrands.length,
-        itemBuilder: (context, index) {
-          return Container(
-            margin: const EdgeInsets.all(10.0),
-            child: Text(perfumeBrands[index]),
-          );
-        },
-        separatorBuilder: (context, index) => const Divider(),
-      )
+
+      // **** Body **** 
+      // on affiche la page en fonction de pageIndex (initialisé à 0)
+      body: pages[pageIndex],
+
+      //  **** Footer **** 
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          border: Border(
+            top: BorderSide(color: Colors.grey, width: 0.2)
+          )
+        ),
+        child: NavigationBar(
+          selectedIndex: pageIndex, // Permet de rendre le bouton actif selon pageIndex
+          onDestinationSelected: (int index) {
+            // mettre à jour le pageIndex
+            setState(() { 
+              pageIndex = index;
+            });
+          },
+          backgroundColor: Colors.white,
+          // Les différents boutons
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.message), label: "Discussion"),
+            NavigationDestination(icon: Icon(Icons.filter_tilt_shift), label: "Actus"),
+            NavigationDestination(icon: Icon(Icons.group), label: "Communautés"),
+            NavigationDestination(icon: Icon(Icons.phone), label: "Appels"),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/pages/communautes.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CommunautesPage extends StatelessWidget {
+  const CommunautesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Communautés page"),
+    );
+  }
+}
